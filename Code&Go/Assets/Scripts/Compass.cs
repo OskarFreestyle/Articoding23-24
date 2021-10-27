a1ee23d454d5b5dcaed73dbb980bf5eb72191db4
Tatiana <tatiandu@ucm.es>
2021-10-28 00:41:58 +0200

    Squashed commit of the following:
    commit 9fadb79b500f903c4a36cde93d58d810217b8c0d
    Author: Tatiana <tatiandu@ucm.es>
    Date:   Thu Oct 28 00:41:36 2021 +0200
    
        Nivel 5 tutoriales arreglado
    
    commit 793265403ffdcd622de01069c82316ab2f2d8ad0
    Merge: f539b0c b8e2617
    Author: Tatiana <tatiandu@ucm.es>
    Date:   Thu Oct 28 00:33:20 2021 +0200
    
        Merge remote-tracking branch 'origin/Brujula' into cambios-varios
    
    commit f539b0c37b6ff9c0122e07db2cbbaf8a011425a1
    Author: Tatiana <tatiandu@ucm.es>
    Date:   Thu Oct 28 00:29:36 2021 +0200
    
        Quitadas las variables que no se usan del xml
    
    commit b8e2617457276a7603bd330dda7dc1661d1b3137
    Author: anmartan <anmartan@ucm.es>
    Date:   Thu Oct 28 00:27:09 2021 +0200
    
        MODIFIED: Nivel tutorial 5
    
        Cambiado de carpeta por coherencia
    
    commit df6a33aabcb9a145470ab5f0814fc57d6f00aca9
    Author: anmartan <anmartan@ucm.es>
    Date:   Thu Oct 28 00:09:13 2021 +0200
    
        MODIFIED: Botón atrás niveles
    
        Cambiado de lugar para mayor legibilidad
    
    commit 754427a9c3c29e795c020448bd471e61a856d1f0
    Author: Tatiana <tatiandu@ucm.es>
    Date:   Wed Oct 27 23:57:51 2021 +0200
    
        Arreglos en popups
    
    commit 3906157eaea62bd431b22a14c023c2282de07c15
    Author: anmartan <anmartan@ucm.es>
    Date:   Wed Oct 27 23:07:21 2021 +0200
    
        ADDED: Pop-up sobre el perfil
    
    commit 192c5984e9548468855879f649fd2d49b7b11820
    Author: Tatiana <tatiandu@ucm.es>
    Date:   Wed Oct 27 22:23:37 2021 +0200
    
        Tutoriales ahora se llama Introduccion
    
    commit 67ed66b6727aab290ed351a73bd94398aab80817
    Author: anmartan <anmartan@ucm.es>
    Date:   Wed Oct 27 22:14:53 2021 +0200
    
        ADDED: Pop-ups explicativos
    
        Pop-ups que explican las categorías que hay en el juego y cómo desbloquearlas.
        Pop-up que explica el funcionamiento de las estrellas.
    
    commit 51557c58342c76d82e2ad6129d50d60fa268f3a7
    Author: anmartan <anmartan@ucm.es>
    Date:   Wed Oct 27 13:48:56 2021 +0200
    
        ADDED: Brújula
    
        Para orientar al jugador durante el nivel

---
+++ Code&Go/Assets/Scripts/Compass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Brujula que sigue la camara en los niveles para marcar el norte
+/// </summary>
+public class Compass : MonoBehaviour
+{
+    public Transform cameraTransform;
+    Vector3 direction;
+
+    private void Start()
+    {
+        direction = new Vector3();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        direction.z = cameraTransform.eulerAngles.y;
+        transform.localEulerAngles = direction;
+    }
+}
