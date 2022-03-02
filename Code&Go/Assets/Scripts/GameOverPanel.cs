16fc63d122ee6aef539e2d5b5a1823b0a86749f7
Tatiana <tatiandu@ucm.es>
2022-03-02 20:02:07 +0100

    FIX: GameOverPanel.cs

--- Code&Go/Assets/Scripts/GameOverPanel.cs
+++ Code&Go/Assets/Scripts/GameOverPanel.cs
@@ -1,31 +1,37 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor.Localization;
 using UnityEngine.Localization.Settings;
+using UnityEngine.Localization.Tables;
 
 public class GameOverPanel : MonoBehaviour
 {
     [SerializeField] private Text _errorText;
-    [SerializeField] private StringTableCollection _stringTableCollection;
+    [SerializeField] private StringTable _stringTableEn;
+    [SerializeField] private StringTable _stringTableEs;
 
     private int _languageCode;
     private void Awake()
     {
-        if (LocalizationSettings.SelectedLocale.Identifier.Code == "en") _languageCode = 0;
-        else _languageCode = 1;
+        if (LocalizationSettings.SelectedLocale.Identifier.Code == "en") _languageCode = 0; //English
+        else _languageCode = 1; //Espanol
     }
 
     public void SetLocalizedText(string entry)
     {
+        //Elige la tabla correspondiente segun el idioma
         try
         {
-            _errorText.text = _stringTableCollection.StringTables[_languageCode][entry].LocalizedValue;
+            if (_languageCode == 0) {
+                _errorText.text = _stringTableEn[entry].LocalizedValue;
+            }
+            else if(_languageCode == 1) {
+                _errorText.text = _stringTableEs[entry].LocalizedValue;
+            }
         }
         catch (Exception e)
         {
             _errorText.gameObject.SetActive(false);
         }
-
     }
 }
