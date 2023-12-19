2b8bfa3c49c7c9daac548a52cb97eb4e29db0c7e
OskarFreestyle <oscafe01@ucm.es>
2023-12-19 18:33:45 +0100

    Finished the carousel view without select button functionality

--- Code&Go/Assets/Scripts/ScriptableObjects/Category.cs
+++ Code&Go/Assets/Scripts/ScriptableObjects/Category.cs
@@ -6,18 +6,28 @@ using UnityEngine.Localization;
 [CreateAssetMenu(fileName = "Category", menuName = "ScriptableObjects/Category")]
 public class Category : ScriptableObject
 {
+    // New
+
     public LocalizedString titleLocalized;
     public LocalizedString descriptionLocalized;
+    
+    public Sprite icon;
 
-    public string name_id;
-    [TextArea(3, 6)]
-    public string description;
+    public Color primaryColor;
+    public Color secondaryColor;
 
     public List<LevelData> levels;
-
-    public Sprite icon;
 
     public int GetTotalStars() {
         return levels.Count * 3;
     }
+
+
+    // Old
+    public string name_id;
+    [TextArea(3, 6)]
+    public string description;
+
+
+
 }
--- Code&Go/Assets/Scripts/UI/CategoryCard.cs
+++ Code&Go/Assets/Scripts/UI/CategoryCard.cs
@@ -7,21 +7,42 @@ using UnityEngine.Localization.Components;
 public class CategoryCard : MonoBehaviour {
     [SerializeField] private Category category;
 
+    [SerializeField] private Image cardBGImage;
+
     [SerializeField] private Text titleText;
-    private LocalizeStringEvent titleLocalized;
+    [SerializeField] private LocalizeStringEvent titleLocalized;
+    [SerializeField] private Image titleBGImage;
 
     [SerializeField] private Text descriptionText;
-    private LocalizeStringEvent descriptionLocalized;
+    [SerializeField] private LocalizeStringEvent descriptionLocalized;
+    [SerializeField] private Image descriptionBGImage;
 
     [SerializeField] private Text numStarsText;
     //private LocalizeStringEvent localizedTitle;
+    [SerializeField] private Image numStarsBGImage;
 
-    [SerializeField] private Image image;
+    [SerializeField] private Image icon;
+    [SerializeField] private Image iconBGImage;
+
+    [SerializeField] private Image buttonBGImage;
 
     /// <summary>
     /// Setup the category using the scriptable object
     /// </summary>
     private void Start() {
+        Debug.Log("STARTED");
+        SetColors();
+        SetTexts();
+        SetIcon();
+        Debug.Log("FINISHED");
+    }
+
+    private void SetIcon() {
+        // Set the current icon
+        icon.sprite = category.icon;
+    }
+
+    private void SetTexts() {
         // Set the title
         titleLocalized.StringReference = category.titleLocalized;
         titleLocalized.RefreshString();
@@ -30,6 +51,21 @@ public class CategoryCard : MonoBehaviour {
         // Set the description
         descriptionLocalized.StringReference = category.descriptionLocalized;
         descriptionLocalized.RefreshString();
-        titleText.text = descriptionLocalized.StringReference.GetLocalizedStringAsync().Result;
+        descriptionText.text = descriptionLocalized.StringReference.GetLocalizedStringAsync().Result;
+
+        // Set the stars number
+        numStarsText.text = "0/" + category.GetTotalStars();
+    }
+
+    private void SetColors() {
+        // Set the primary color
+        cardBGImage.color = category.primaryColor;
+
+        // Set the secondary color
+        titleBGImage.color = category.secondaryColor;
+        descriptionBGImage.color = category.secondaryColor;
+        numStarsBGImage.color = category.secondaryColor;
+        buttonBGImage.color = category.secondaryColor;
+        iconBGImage.color = category.secondaryColor;
     }
 }
