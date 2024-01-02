9aed0e0d7320388b091b4fafedb021eda7be6faf
OskarFreestyle <oscafe01@ucm.es>
2024-01-02 21:43:09 +0100

    Special case levels created category finished

--- Code&Go/Assets/Scripts/UI/CategoryCard.cs
+++ Code&Go/Assets/Scripts/UI/CategoryCard.cs
@@ -28,6 +28,7 @@ public class CategoryCard : MonoBehaviour {
     [SerializeField] private Image numStarsBGImage;
 
     [SerializeField] private Image icon;
+    [SerializeField] private Image lockedIcon;
     [SerializeField] private Image iconBGImage;
 
     [SerializeField] private Image buttonBGImage;
@@ -42,6 +43,7 @@ public class CategoryCard : MonoBehaviour {
     private const int EXPAND_NUM_STARS_POS_X = 10;
     private const int EXPAND_NUM_STARS_POS_Y = -330;
     private const int EXPAND_BUTTON_POS_X = -300;
+    private const int EXPAND_CREATED_LEVEL_BUTTON_POS_X = -145;
     private const int EXPAND_BUTTON_POS_Y = -330;    
     private const int EXPAND_DESCRIPTION_POS_X = -300;
     private const int EXPAND_DESCRIPTION_POS_Y = -240;
@@ -53,6 +55,7 @@ public class CategoryCard : MonoBehaviour {
     private const float CONTRACT_TEXTS_SCALE = 0.6f;
     private const float CONTRACT_ICON_SCALE = 0.8f;
     private const float CONTRACT_DESCRIPTION_SCALE = 0.25f;
+    private const float CONTRACT_CREATED_LEVELS_DESCRIPTION_SCALE = 0f;
     private const int CONTRACT_NUM_STARS_POS_X = -78;
     private const int CONTRACT_NUM_STARS_POS_Y = -199;    
     private const int CONTRACT_BUTTON_POS_X = -78;
@@ -97,6 +100,9 @@ public class CategoryCard : MonoBehaviour {
         descriptionLocalized.RefreshString();
         descriptionText.text = descriptionLocalized.StringReference.GetLocalizedStringAsync().Result;
 
+        // The created levels category doesn't have stars
+        if (category.index == 0) numStarsBGImage.gameObject.SetActive(false);
+
         //// Set the stars number by default
         //numStarsText.text = "0/" + category.GetTotalStars();
     }
@@ -131,7 +137,12 @@ public class CategoryCard : MonoBehaviour {
         // Scale, move and enable the button
         buttonBGImage.GetComponent<Button>().enabled = true;
         DOTween.To(() => buttonBGImage.GetComponent<RectTransform>().localScale, x => buttonBGImage.GetComponent<RectTransform>().localScale = x, new Vector3(EXPAND_TEXTS_SCALE, EXPAND_TEXTS_SCALE), adjustSpeed);
-        DOTween.To(() => buttonBGImage.GetComponent<RectTransform>().localPosition, x => buttonBGImage.GetComponent<RectTransform>().localPosition = x, new Vector3(EXPAND_BUTTON_POS_X, EXPAND_BUTTON_POS_Y), adjustSpeed);
+        if(category.index != 0) {
+            DOTween.To(() => buttonBGImage.GetComponent<RectTransform>().localPosition, x => buttonBGImage.GetComponent<RectTransform>().localPosition = x, new Vector3(EXPAND_BUTTON_POS_X, EXPAND_BUTTON_POS_Y), adjustSpeed);
+        }
+        else {
+            DOTween.To(() => buttonBGImage.GetComponent<RectTransform>().localPosition, x => buttonBGImage.GetComponent<RectTransform>().localPosition = x, new Vector3(EXPAND_CREATED_LEVEL_BUTTON_POS_X, EXPAND_BUTTON_POS_Y), adjustSpeed);
+        }
 
         // Scale and move the num stars
         DOTween.To(() => numStarsBGImage.GetComponent<RectTransform>().localScale, x => numStarsBGImage.GetComponent<RectTransform>().localScale = x, new Vector3(EXPAND_TEXTS_SCALE, EXPAND_TEXTS_SCALE), adjustSpeed);
@@ -148,30 +159,42 @@ public class CategoryCard : MonoBehaviour {
         // Scale and move the icon
         DOTween.To(() => iconBGImage.GetComponent<RectTransform>().localScale, x => iconBGImage.GetComponent<RectTransform>().localScale = x, new Vector3(CONTRACT_ICON_SCALE, CONTRACT_ICON_SCALE), adjustSpeed);
         DOTween.To(() => iconBGImage.GetComponent<RectTransform>().localPosition, x => iconBGImage.GetComponent<RectTransform>().localPosition = x, new Vector3(CONTRACT_ICON_POS_X, CONTRACT_ICON_POS_Y), adjustSpeed);
-        
+
         // Scale and move the description
-        DOTween.To(() => descriptionBGImage.GetComponent<RectTransform>().localScale, x => descriptionBGImage.GetComponent<RectTransform>().localScale = x, new Vector3(CONTRACT_DESCRIPTION_SCALE, CONTRACT_DESCRIPTION_SCALE), adjustSpeed / 2);
-        DOTween.To(() => descriptionBGImage.GetComponent<RectTransform>().localPosition, x => descriptionBGImage.GetComponent<RectTransform>().localPosition = x, new Vector3(CONTRACT_DESCRIPTION_POS_X, CONTRACT_DESCRIPTION_POS_Y), adjustSpeed / 2);
+        if (category.index != 0) {
+            DOTween.To(() => descriptionBGImage.GetComponent<RectTransform>().localScale, x => descriptionBGImage.GetComponent<RectTransform>().localScale = x, new Vector3(CONTRACT_DESCRIPTION_SCALE, CONTRACT_DESCRIPTION_SCALE), adjustSpeed / 2);
+            DOTween.To(() => descriptionBGImage.GetComponent<RectTransform>().localPosition, x => descriptionBGImage.GetComponent<RectTransform>().localPosition = x, new Vector3(CONTRACT_DESCRIPTION_POS_X, CONTRACT_DESCRIPTION_POS_Y), adjustSpeed / 2);
+        }
+        else {
+            DOTween.To(() => descriptionBGImage.GetComponent<RectTransform>().localScale, x => descriptionBGImage.GetComponent<RectTransform>().localScale = x, new Vector3(CONTRACT_CREATED_LEVELS_DESCRIPTION_SCALE, CONTRACT_CREATED_LEVELS_DESCRIPTION_SCALE), adjustSpeed / 2);
+            DOTween.To(() => descriptionBGImage.GetComponent<RectTransform>().localPosition, x => descriptionBGImage.GetComponent<RectTransform>().localPosition = x, new Vector3(CONTRACT_CREATED_LEVELS_DESCRIPTION_SCALE, CONTRACT_DESCRIPTION_POS_Y), adjustSpeed / 2);
+        }
 
         // Scale, move and enable the button
         buttonBGImage.GetComponent<Button>().enabled = false;
-        DOTween.To(() => buttonBGImage.GetComponent<RectTransform>().localScale, x => buttonBGImage.GetComponent<RectTransform>().localScale = x, new Vector3(CONTRACT_TEXTS_SCALE, CONTRACT_TEXTS_SCALE), adjustSpeed / 2);
-        DOTween.To(() => buttonBGImage.GetComponent<RectTransform>().localPosition, x => buttonBGImage.GetComponent<RectTransform>().localPosition = x, new Vector3(CONTRACT_BUTTON_POS_X, CONTRACT_BUTTON_POS_Y), adjustSpeed / 2);
-
+        if (category.index != 0) {
+            DOTween.To(() => buttonBGImage.GetComponent<RectTransform>().localPosition, x => buttonBGImage.GetComponent<RectTransform>().localPosition = x, new Vector3(CONTRACT_BUTTON_POS_X, CONTRACT_BUTTON_POS_Y), adjustSpeed / 2);
+            DOTween.To(() => buttonBGImage.GetComponent<RectTransform>().localScale, x => buttonBGImage.GetComponent<RectTransform>().localScale = x, new Vector3(CONTRACT_TEXTS_SCALE, CONTRACT_TEXTS_SCALE), adjustSpeed / 2);
+        }
+        else {
+            DOTween.To(() => buttonBGImage.GetComponent<RectTransform>().localPosition, x => buttonBGImage.GetComponent<RectTransform>().localPosition = x, new Vector3(CONTRACT_CREATED_LEVELS_DESCRIPTION_SCALE, CONTRACT_BUTTON_POS_Y), adjustSpeed / 2);
+            DOTween.To(() => buttonBGImage.GetComponent<RectTransform>().localScale, x => buttonBGImage.GetComponent<RectTransform>().localScale = x, new Vector3(CONTRACT_CREATED_LEVELS_DESCRIPTION_SCALE, CONTRACT_CREATED_LEVELS_DESCRIPTION_SCALE), adjustSpeed / 2);
+        }
         // Scale and move the num stars
         DOTween.To(() => numStarsBGImage.GetComponent<RectTransform>().localScale, x => numStarsBGImage.GetComponent<RectTransform>().localScale = x, new Vector3(CONTRACT_TEXTS_SCALE, CONTRACT_TEXTS_SCALE), adjustSpeed);
         DOTween.To(() => numStarsBGImage.GetComponent<RectTransform>().localPosition, x => numStarsBGImage.GetComponent<RectTransform>().localPosition = x, new Vector3(CONTRACT_NUM_STARS_POS_X, CONTRACT_NUM_STARS_POS_Y), adjustSpeed);
     }
 
     public void Configure(CategorySaveData categoryData) {
+        // The created levels category doesn't need to be configured
+        if (category.index == 0) return;
+
         // Update the number of stars
         numStarsText.text = categoryData.GetCurrentNumStars().ToString() + "/" + category.GetTotalStars().ToString();
 
         // Update the icon
-
-
-
-        Debug.Log(gameObject.name + " configured");
-        Debug.Log(gameObject.name + " has " + categoryData.GetCurrentNumStars().ToString());
+        lockedIcon.gameObject.SetActive(categoryData.levelsData[0] == -1);
+        icon.gameObject.SetActive(categoryData.levelsData[0] != -1);
+        buttonBGImage.GetComponent<Button>().enabled = categoryData.levelsData[0] != -1;
     }
 }
